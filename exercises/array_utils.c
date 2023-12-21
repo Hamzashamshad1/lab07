2c7f3e6c3a9c16dfea5bc42f49d74be6587e93fc
hamzashamshad136020 <hamzashamshad136020@gmail.com>
2023-12-21 23:01:40 +0800

    max function

--- exercises/array_utils.c
+++ exercises/array_utils.c
@@ -94,11 +94,18 @@ int getIndexOfMin(const int *arr, int size) {
             index++;
         }
     }
-    return index;
+    return ;
 }
 
 int getMax(const int *arr, int size) {
-  //TODO: implement
+   int max = arr[0];
+
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
 }
 
 int getIndexOfMax(const int *arr, int size) {
